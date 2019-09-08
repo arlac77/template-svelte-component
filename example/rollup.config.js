0e6485625561437cae79067bf7840dd7138e5ccc
Markus Felten <Markus.Felten@gmx.de>
2019-09-08 03:59:07 +0200

    chore: switch to rollup-plugin-dev

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,26 +1,9 @@
+import dev from "rollup-plugin-dev";
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
-import livereload from "rollup-plugin-livereload";
-import http from "http";
-import handler from "serve-handler";
 import pkg from "../package.json";
 
 const port = pkg.config.port || 5000;
-
-const development = process.env.ROLLUP_WATCH;
-
-if (development) {
-  const server = http.createServer((request, response) => {
-    return handler(request, response, {
-      public: "example/public",
-      rewrites: [{ source: "**", destination: "/index.html" }]
-    });
-  });
-
-  server.listen(port, () => {
-    console.log(`Running at http://localhost:${port}`);
-  });
-}
 
 export default {
   input: "example/src/index.mjs",
@@ -30,8 +13,13 @@ export default {
     file: `example/public/bundle.mjs`
   },
   plugins: [
+    dev({
+      port,
+      dirs: ["example/public"],
+      spa: "example/public/index.html",
+      basePath: "/base"
+    }),
     resolve({ browser: true }),
-    svelte(),
-    development && livereload("example/public")
+    svelte()
   ]
 };
